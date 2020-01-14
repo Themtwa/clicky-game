3ccfcd238405d19602e6fe00c4a0665937b5d445
Matthew Tarbet <goatdaddy08@yahoo.com>
2020-01-14 02:27:51 -0700

    heroku deply

---
+++ build/precache-manifest.a05a12d4cfb167efeb6b39e737e28d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8ebbf54d9353654562b81c7f018250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "559927ee3ba388a7946f",
+    "url": "/static/css/main.5cb3dd4c.chunk.css"
+  },
+  {
+    "revision": "f49d31ae12388dff11ba",
+    "url": "/static/js/2.fc849696.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fc849696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "559927ee3ba388a7946f",
+    "url": "/static/js/main.079c8c16.chunk.js"
+  },
+  {
+    "revision": "6c294ec4e22eb8cd4a3d",
+    "url": "/static/js/runtime-main.d5fc835f.js"
+  }
+]);
