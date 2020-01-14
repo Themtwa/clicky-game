3f7972c7bc67a9c421b5e72b3ae7e7632e557b8b
Matthew Tarbet <goatdaddy08@yahoo.com>
2020-01-14 02:43:05 -0700

    back to github

---
+++ build/precache-manifest.57a554bda446eb271b90ddf75a17f41a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72c73c7cfa6976563e2a3594c2ebd21",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "559927ee3ba388a7946f",
+    "url": "/clicky-game/static/css/main.5cb3dd4c.chunk.css"
+  },
+  {
+    "revision": "f49d31ae12388dff11ba",
+    "url": "/clicky-game/static/js/2.fc849696.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.fc849696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "559927ee3ba388a7946f",
+    "url": "/clicky-game/static/js/main.079c8c16.chunk.js"
+  },
+  {
+    "revision": "6a4a0ce30c34487ac351",
+    "url": "/clicky-game/static/js/runtime-main.0ef93b42.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a05a12d4cfb167efeb6b39e737e28d2c.js"
+  "/clicky-game/precache-manifest.57a554bda446eb271b90ddf75a17f41a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
